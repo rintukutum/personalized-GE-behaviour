0ff55215076fde1a10a09f62f4ce101844e4871f
rintukutum <rintukutum@gmail.com>
2019-06-11 23:39:05 +0530

    added codes and results

---
+++ 01-DC-MTB-12.R
@@ -0,0 +1,193 @@
+rm(list=ls())
+load('./data/processed/DC-MTB-12/dc.mtb12.freezed.RData')
+load('./data/raw/annotation/DC-MTB-12/dc.mtb12.geneSymbol.RData')
+probeIDs <- dc.mtb12.geneSymbol$Probe_Id
+dc.temp <- dc.mtb12.freezed
+dc.temp$R1$`MTB-infected` <- dc.temp$R1$`MTB-infected`[probeIDs,]
+dc.temp$R1$`non-infected` <- dc.temp$R1$`non-infected`[probeIDs,]
+
+dc.temp$R2$`MTB-infected` <- dc.temp$R2$`MTB-infected`[probeIDs,]
+dc.temp$R2$`non-infected` <- dc.temp$R2$`non-infected`[probeIDs,]
+
+rm(dc.mtb12.freezed)
+probeID.anno <- dc.mtb12.geneSymbol[probeIDs,c('Probe_Id','Symbol','Entrez_Gene_ID')]
+rownames(probeID.anno) <- 1:nrow(probeID.anno)
+dc.mtb12.working <- list(
+  data = dc.temp,
+  annotation = probeID.anno
+)
+save(dc.mtb12.working,
+     file = './data/processed/DC-MTB-12/dc.mtb12.working.RData'
+)
+rm(list=ls())
+load('./data/processed/DC-MTB-12/dc.mtb12.working.RData')
+
+R1.inf <- dc.mtb12.working$data$R1$`MTB-infected`
+R1.noninf <- dc.mtb12.working$data$R1$`non-infected`
+
+delta <- R1.inf - R1.noninf
+
+responsivity.data <- cbind(R1.noninf, delta)
+resV.cor <- apply(
+  responsivity.data,
+  1,
+  function(x){
+    x <- as.numeric(x)
+    h.x <- length(x)/2
+    out <- cor(x[1:h.x],x[(h.x+1):length(x)])
+    }
+)
+
+response.data <- cbind(R1.inf, delta)
+reS.cor <- apply(
+  response.data,
+  1,
+  function(x){
+    x <- as.numeric(x)
+    h.x <- length(x)/2
+    out <- cor(x[1:h.x],x[(h.x+1):length(x)])
+  }
+)
+dir.create('./figures/',showWarnings = FALSE)
+dir.create('./figures/DC-MTB-12',showWarnings = FALSE)
+png('./figures/DC-MTB-12/01-histogram-DC-MTB-12__R1.png',width = 1200,height = 800,
+    res = 150)
+par(mfrow=c(1,2))
+hist(resV.cor,main = 'Responsivity genes',xlab = 'Correlation')
+hist(reS.cor,main = 'Response genes',xlab = 'Correlation')
+dev.off()
+stats.mtb <- c(table(abs(resV.cor) >= 0.8)['TRUE'],
+  table(abs(reS.cor) >= 0.8)['TRUE']
+)
+names(stats.mtb) <- c('responsivity','response')
+png('./figures/DC-MTB-12/01-Statistics-DC-MTB-12__R1.png',width = 1200,height = 800,
+    res = 150)
+par(mfrow=c(1,1))
+barplot(stats.mtb,ylab = '# probes(Genes)',main = 'DC-MTB-2012 | R1')
+dev.off()
+
+#####
+which.max(abs(resV.cor)) # LAMP3
+which.max(abs(reS.cor)) # CXCR7
+
+#####
+extractCorAnno <- function(cor,threshold=0.8){
+   probes.cor <- cor[abs(cor) >= threshold]
+   idx <- dc.mtb12.working$annotation$Probe_Id %in% names(probes.cor)
+   genes.cor <- dc.mtb12.working$annotation[idx,]
+   genes.cor$cor <- as.numeric(probes.cor[genes.cor$Probe_Id])
+   return(genes.cor)
+}
+
+R1.responsivity.genes <- extractCorAnno(
+   cor = resV.cor
+)
+R1.response.genes <- extractCorAnno(
+   cor = reS.cor
+)
+
+#####
+R2.inf <- dc.mtb12.working$data$R2$`MTB-infected`
+R2.noninf <- dc.mtb12.working$data$R2$`non-infected`
+
+R2.delta <- R2.inf - R2.noninf
+
+R2.responsivity.data <- cbind(R2.noninf, R2.delta)
+R2.resV.cor <- apply(
+  R2.responsivity.data,
+  1,
+  function(x){
+    x <- as.numeric(x)
+    h.x <- length(x)/2
+    out <- cor(x[1:h.x],x[(h.x+1):length(x)])
+  }
+)
+
+R2.response.data <- cbind(R2.inf, R2.delta)
+R2.reS.cor <- apply(
+  R2.response.data,
+  1,
+  function(x){
+    x <- as.numeric(x)
+    h.x <- length(x)/2
+    out <- cor(x[1:h.x],x[(h.x+1):length(x)])
+  }
+)
+
+
+png('./figures/DC-MTB-12/01-histogram-DC-MTB-12__R2.png',width = 1200,height = 800,
+    res = 150)
+par(mfrow=c(1,2))
+hist(R2.resV.cor,main = 'Responsivity genes',xlab = 'Correlation')
+hist(R2.reS.cor,main = 'Response genes',xlab = 'Correlation')
+dev.off()
+R2.stats.mtb <- c(table(abs(R2.resV.cor) >= 0.8)['TRUE'],
+               table(abs(R2.reS.cor) >= 0.8)['TRUE']
+)
+names(stats.mtb) <- c('responsivity','response')
+png('./figures/DC-MTB-12/01-Statistics-DC-MTB-12__R2.png',width = 1200,height = 800,
+    res = 150)
+par(mfrow=c(1,1))
+barplot(R2.stats.mtb,ylab = '# probes(Genes)',main = 'DC-MTB-2012 | R2')
+dev.off()
+
+which.max(abs(R2.resV.cor)) # LAMP3 # ILMN_2170814
+which.max(abs(R2.reS.cor)) # CXCR7 # ILMN_1798360
+
+R2.responsivity.genes <- extractCorAnno(
+   cor = R2.resV.cor
+)
+R2.response.genes <- extractCorAnno(
+   cor = R2.reS.cor
+)
+dc.mtb.12.profile <- list(
+   responsivity = rbind(
+     data.frame(
+         R1.responsivity.genes,
+         replicate = 'R1',
+         stringsAsFactors = FALSE
+     ),
+     data.frame(
+         R2.responsivity.genes,
+         replicate = 'R2',
+         stringsAsFactors = FALSE
+     )
+     ),
+     response = rbind(
+     data.frame(
+         R1.response.genes,
+         replicate = 'R1',
+         stringsAsFactors = FALSE
+     ),
+     data.frame(
+         R2.response.genes,
+         replicate = 'R2',
+         stringsAsFactors = FALSE
+     )
+     )
+)
+dir.create('./data/results/',showWarnings=FALSE)
+dir.create('./data/results/DC-MTB-12/',showWarnings=FALSE)
+
+
+save(
+  dc.mtb.12.profile,
+  file = './data/results/DC-MTB-12/dc.mtb.12.profile.RData'
+)
+
+png('./figures/DC-MTB-12/01-Replicate-results-LAMP3.png',
+    width = 1200,height = 800,res = 150)
+par(mfrow=c(1,2))
+plot(y = delta['ILMN_2170814',],
+     x = R1.noninf['ILMN_2170814',],
+     ylab='delta',
+     xlab = 'baseline',
+     main = 'R1 | ILMN_2170814, LAMP3')
+
+plot(y = R2.delta['ILMN_2170814',],
+     x = R2.noninf['ILMN_2170814',],
+     ylab='delta',
+     xlab = 'baseline',
+     main = 'R2 | ILMN_2170814, LAMP3')
+
+dev.off()
---
+++ 02-MONO-IFN-LIPO-14.R
@@ -0,0 +1,2 @@
+rm(list=ls())
+load('./data/processed/MONO-IFN-LIPO-14/mono.ifn.data.RData')
